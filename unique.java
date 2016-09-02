f2ab49ed05b2831f5319e4ac1660390c2d776f4e
NeethuVadivel <neethuvadivel@gmail.com>
2016-09-02 10:44:50 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int [] a=new int[n];
+		int i=0,c=0;
+		for(i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		for(int j=0;j<n;j++){
+			c=0;
+			for(i=0;i<n;i++){
+				if(a[j]==a[i]){
+					c++;
+				}
+			}
+			if(c==1){
+				System.out.println(a[j]);
+			}
+		}
+	}
+}
